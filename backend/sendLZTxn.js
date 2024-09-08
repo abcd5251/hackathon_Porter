e8b0a252782e304bf22505a95feb90d39feda568
Allen Chu <48011076+abcd5251@users.noreply.github.com>
2024-09-09 00:19:56 +0800

    Update sendLZTxn.js

--- backend/sendLZTxn.js
+++ backend/sendLZTxn.js
@@ -16,13 +16,13 @@ const contractABI = JSON.parse(fs.readFileSync(abiPath, "utf8"));
 console.log(chainConfigs);
 
 let call = {
-    dstEid: 101, // 目標鏈的 Endpoint ID (假設是 101 表示目標鏈)
-    target: "0xTargetContractAddress", // 目標合約的地址
-    calldata: "yourCalldata", // 使用 web3.js 或 ethers.js 編碼 calldata
-    option: "0x", // 例如，默認選項 (沒有具體選項時設為空)
+    dstEid: 101, // target chain Endpoint ID 
+    target: "0xTargetContractAddress", 
+    calldata: "yourCalldata", 
+    option: "0x",
     fee: {
-        nativeFee: ethers.utils.parseEther("0.01"), // 使用本地代幣支付的費用，例如 0.01 ETH
-        lzTokenFee: ethers.utils.parseUnits("10", 18) // 使用 LayerZero 代幣支付的費用，例如 10 LZ Token?
+        nativeFee: ethers.utils.parseEther("0.01"), 
+        lzTokenFee: ethers.utils.parseUnits("10", 18) 
     }
 }
 
@@ -71,7 +71,7 @@ async function procesCall(crossChainData, targetChainName) {
 async function sendCrossChainTxn(targetNbr) {
     const provider = new ethers.providers.JsonRpcProvider(chainConfigs[targetNbr].rpc);
 
-    // 使用私鑰創建一個錢包對象來簽署交易
+    // use private key to sign a transaction
     const wallet = new ethers.Wallet("3e1c59de146cfa09fff46f82c4502ae57387093379c857555800f9cb8cbdbb3b", provider);
     
     const contract = new ethers.Contract(chainConfigs[targetNbr].contract, contractABI, wallet);
@@ -80,8 +80,7 @@ async function sendCrossChainTxn(targetNbr) {
         console.log(calls)
         const tx = await contract.send(calls, {value: ethers.utils.parseEther("0")}); // 可以設置 value 為跨鏈消息支付的額外費用
         console.log("Transaction sent, hash:", tx.hash);
-
-        // 等待交易確認
+        
         const receipt = await tx.wait();
         console.log("Transaction confirmed, receipt:", receipt);
     } catch (error) {
@@ -92,7 +91,7 @@ async function sendCrossChainTxn(targetNbr) {
 procesCall(crossChainData, "Sepolia");
 
 
-// // 監聽每個合約的 ApproveOp 事件
+// // listen events
 // providers.forEach(({ name, contract }) => {
 //   contract.on('ApproveOp', (target, calldataHash, calldata) => {
 //     console.log(`${name}: Event received - Target: ${target}, Calldata Hash: ${calldataHash}`);
@@ -100,18 +99,18 @@ procesCall(crossChainData, "Sepolia");
 //     console.log("Target Address:", target);
 //     console.log("Calldata Hash:", calldataHash);
 //     console.log("Calldata:", calldata);
-//     // 處理事件並生成 UserOperation
+//     // handle UserOperation
 //     sendUserOperation(contract, target, calldata);
 //   });
 // const call = [
 //     {
-//         dstEid: 101, // 目標鏈的 Endpoint ID (假設是 101 表示目標鏈)
-//         target: "0xTargetContractAddress", // 目標合約的地址
-//         calldata_: yourCalldata, // 使用 web3.js 或 ethers.js 編碼 calldata
-//         option: "0x", // 例如，默認選項 (沒有具體選項時設為空)
+//         dstEid: 101, // target chain Endpoint ID 
+//         target: "0xTargetContractAddress", // target address
+//         calldata_: yourCalldata, // use web3.js
+//         option: "0x", 
 //         fee: {
-//             nativeFee: ethers.utils.parseEther("0.01"), // 使用本地代幣支付的費用，例如 0.01 ETH
-//             lzTokenFee: ethers.utils.parseUnits("10", 18) // 使用 LayerZero 代幣支付的費用，例如 10 LZ Token
+//             nativeFee: ethers.utils.parseEther("0.01"), 
+//             lzTokenFee: ethers.utils.parseUnits("10", 18) 
 //         }
 //     }
 // ];
